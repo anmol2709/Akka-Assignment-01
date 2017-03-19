cd5922d3bc2b8f004e66f516f053bf29c8ad7e2d
anmol sarna <anmol sarna>
2017-03-19 23:59:16 +0530

    'First-commit-without-testing'

---
+++ src/main/scala/FileHandling.scala
@@ -0,0 +1,63 @@
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.ask
+import akka.routing.RoundRobinPool
+import akka.util.Timeout
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.io.Source
+
+
+class ChildFileHandlingActor extends Actor {
+
+
+  override def receive: Receive = {
+    case msg: String =>
+      val sizeOfLine = msg.split("[ ,!.]+").size
+      sender() ! sizeOfLine
+  }
+}
+
+class FileHandlingActor extends Actor {
+
+  implicit val timeout = Timeout(1000 seconds)
+
+  val system = ActorSystem("RouterSystem")
+  val childRouter = context.actorOf(RoundRobinPool(5).props(Props[ChildFileHandlingActor]), name = "childPoolRouter")
+
+  val system2 = ActorSystem("RouterSystem")
+  val router = system2.actorOf(Props[FileHandlingActor])
+  var countOfWords = 0
+
+  override def receive: Receive = {
+    case msg: String =>
+      try {
+        val fileData = Source.fromFile(msg).getLines().toList
+        val lines = (for {
+          i <- 0 to fileData.size - 1
+        } yield childRouter ? (fileData(i).toString)).toList
+        val listOfLineSize = Await.result(Future.sequence(lines), 10 seconds)
+
+        listOfLineSize.map {
+          case x: Int => countOfWords = countOfWords + x
+        }
+        sender() ! countOfWords
+      }
+      catch{
+        case exp: Exception => println("No such file exists")
+      }
+  }
+}
+
+object FileHandling extends App {
+  implicit val timeout = Timeout(1000 seconds)
+
+  val system = ActorSystem("RouterSystem")
+  val router = system.actorOf(Props[FileHandlingActor])
+
+   // Taking a demo file from rsources
+  val fileName = "./src/main/resources/DemoFile.txt"
+    val res = router ? fileName
+  res map (x=>print("Number of Words In File Are : " + x))
+
+}
---
+++ src/main/scala/TicketBooking.scala
@@ -0,0 +1,78 @@
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+
+
+/**
+  * Created by ANMOL on 19-03-2017.
+  */
+
+class Person extends Actor {
+  override def receive: Receive = {
+    case num: Any => {
+    }
+  }
+}
+
+class BookingReception extends Actor {
+  var remainingSeats = 6;
+
+  override def receive: Receive = {
+    case num: Int =>
+      println(s"* REQUEST FROM : ${sender().path.name.toUpperCase}")
+      implicit val timeout = Timeout(10 seconds)
+      if (num <= remainingSeats) {
+        println(s"-- Seats Available :  $remainingSeats")
+        remainingSeats -= num
+        println(s"-- Seats Booked: $num ")
+        println(s"-- Seats Available Now :  $remainingSeats \n")
+      }
+      else {
+        println(s"-- Seats Available :  $remainingSeats")
+        println(s"-- $num Seats Not Available!! ")
+        println(s"-- Seats Available Now :  $remainingSeats \n")
+      }
+  }
+}
+
+
+class BookingQueue extends Actor {
+
+  override def receive: Receive = {
+    case request: Int =>
+      val bookingReception = context.actorSelection("../Reception")
+      bookingReception forward (request)
+  }
+}
+
+
+object TicketBooking extends App {
+  implicit val timeout = Timeout(10 seconds)
+
+  val system = ActorSystem("TicketBooking")
+
+  val propsPerson = Props[Person]
+
+  val reception = system.actorOf(Props[BookingReception], "Reception")
+  val bookingQueue1 = system.actorOf(Props[BookingQueue])
+  val bookingQueue2 = system.actorOf(Props[BookingQueue])
+  val bookingQueue3 = system.actorOf(Props[BookingQueue])
+
+  val person1 = system.actorOf(propsPerson, "Person1")
+  val person2 = system.actorOf(propsPerson, "Person2")
+  val person3 = system.actorOf(propsPerson, "Person3")
+  val person4 = system.actorOf(propsPerson, "Person4")
+  val person5 = system.actorOf(propsPerson, "Person5")
+  val person6 = system.actorOf(propsPerson, "Person6")
+
+  bookingQueue1.tell(1, person1)
+  bookingQueue2.tell(2, person2)
+  bookingQueue1.tell(3, person3)
+  bookingQueue3.tell(4, person4)
+  bookingQueue1.tell(1, person5)
+  bookingQueue3.tell(2, person6)
+
+}
+
+
